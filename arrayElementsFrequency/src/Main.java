4eec84c0ab6ba8d5617fde01eb57ed6eff4e5f2c
mcancankaya <mccankaya45@hotmail.com>
2022-11-21 03:29:18 +0300

    array elements frequency

--- arrayElementsFrequency/src/Main.java
+++ arrayElementsFrequency/src/Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Main {
 
-    static boolean isFind(int[] array, int value,int x) {
-        for (int i=0; i<x;i++) {
+    static boolean isFind(int[] array, int value, int x) {
+        for (int i = 0; i < x; i++) {
             if (value == array[i]) {
                 return false;
             }
@@ -21,12 +21,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] numbers = {86,68,92,18,5,18,29,68,86,92,86,5,18,29,30,30,30};
+        int[] numbers = {86, 68, 92, 18, 5, 18, 29, 68, 86, 92, 86, 5, 18, 29, 30, 30, 30};
         System.out.println("Array : " + Arrays.toString(numbers));
         System.out.println("Repeating numbers and their quantities");
         for (int i = 0; i < numbers.length; i++) {
-            if (isFind(numbers, numbers[i],i)) {
-                System.out.println("Number " + numbers[i] +", repeated "+  equalCount(numbers, numbers[i]) + " times.");
+            if (isFind(numbers, numbers[i], i)) {
+                System.out.println("Number " + numbers[i] + ", repeated " + equalCount(numbers, numbers[i]) + " times.");
             }
         }
     }
