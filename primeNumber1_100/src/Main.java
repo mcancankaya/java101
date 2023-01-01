da9150369609bec2c5e0f83eeee5942cfa80d050
Can <mccankaya45@hotmail.com>
2023-01-01 15:49:57 +0300

    number of prime

---
+++ primeNumber1_100/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isPrime;
+        for (int i = 2; i <= 100; i++) {
+            isPrime=true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+}
